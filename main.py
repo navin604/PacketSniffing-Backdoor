1c1a0d0285b4c3a01ed8ad999b6d28ddab8a5542
Navin Parmar <navinparmar98@gmail.com>
2023-05-20 13:19:35 -0700

    Added mode selection and initialization of client/backdoor class

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 from client import Client
 from back_door import BackDoor
+import sys
+
+
+def shutdown():
+    print("You must specify one argument -> CLIENT or SERVER")
+    sys.exit()
+
+
+def process_arg(arg):
+    if len(arg) != 1:
+        shutdown()
+    elif arg[0] == "SERVER":
+        return True
+    elif arg[0] == "CLIENT":
+        return False
+    else:
+        shutdown()
+
+
+def run_server():
+    B = BackDoor()
+
+
+def run_client():
+    C = Client()
+
 
 def main():
-    pass
+    mode = process_arg(sys.argv[1:])
+    if mode:
+        run_server()
+    else:
+        run_client()
 
 
 if __name__ == "__main__":
